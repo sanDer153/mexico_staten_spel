a8c3325ab0b233344cffab74e7c45e54dcf4a29b
Sander Verwimp <91965164+sanDer153@users.noreply.github.com>
2023-07-15 16:34:15 +0200

    add map interaction

--- index.js
+++ index.js
@@ -1,1 +1,81 @@
-console.log("js loaded");
+const resources = ["Hout", "Graan", "Steen", "Kippen", "Goud"];
+const factions = ["Maya's", "Azteken"];
+
+const currentMapState = {
+  "baja-california": {
+    name: "Baja California",
+    faction: "None",
+    resource: "None",
+  },
+  "baja-california-sur": {
+    name: "Baja California Sur",
+    faction: "None",
+    resource: "None",
+  },
+  coahuila: {
+    name: "Coahuila",
+    faction: "None",
+    resource: "None",
+  },
+  chihuahua: {
+    name: "Chihuahua",
+    faction: "None",
+    resource: "None",
+  },
+  durango: {
+    name: "Durango",
+    faction: "None",
+    resource: "None",
+  },
+  sinaloa: {
+    name: "Sinaloa",
+    faction: "None",
+    resource: "None",
+  },
+  sonora: {
+    name: "Sonora",
+    faction: "None",
+    resource: "None",
+  },
+  zacatecas: {
+    name: "Zacatecas",
+    faction: "None",
+    resource: "None",
+  },
+  "nuevo-león": {
+    name: "Nuevo León",
+    faction: "None",
+    resource: "None",
+  },
+  chihuahua: {
+    name: "Chihuahua",
+    faction: "None",
+    resource: "None",
+  },
+  chihuahua: {
+    name: "Chihuahua",
+    faction: "None",
+    resource: "None",
+  },
+  chihuahua: {
+    name: "Chihuahua",
+    faction: "None",
+    resource: "None",
+  },
+  chihuahua: {
+    name: "Chihuahua",
+    faction: "None",
+    resource: "None",
+  },
+
+  chihuahua: {
+    name: "Chihuahua",
+    faction: "None",
+    resource: "None",
+  },
+  chihuahua: {
+    name: "Chihuahua",
+    faction: "None",
+    resource: "None",
+  },
+};
