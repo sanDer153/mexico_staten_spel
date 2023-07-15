e10a3f37ebe41b72dffa88ac4519608fd6647813
Sander Verwimp <91965164+sanDer153@users.noreply.github.com>
2023-07-15 17:59:12 +0200

    add behaviour for selecting states

--- index.js
+++ index.js
@@ -47,35 +47,142 @@ const currentMapState = {
     faction: "None",
     resource: "None",
   },
-  chihuahua: {
-    name: "Chihuahua",
+  "san-luis-potosí": {
+    name: "San Luis Potosí",
     faction: "None",
     resource: "None",
   },
-  chihuahua: {
-    name: "Chihuahua",
+  tamaulipas: {
+    name: "Tamaulipas",
     faction: "None",
     resource: "None",
   },
-  chihuahua: {
-    name: "Chihuahua",
+  aguascalientes: {
+    name: "Aguascalientes",
     faction: "None",
     resource: "None",
   },
-  chihuahua: {
-    name: "Chihuahua",
+  colima: {
+    name: "Colima",
     faction: "None",
     resource: "None",
   },
-
-  chihuahua: {
-    name: "Chihuahua",
+  jalisco: {
+    name: "Jalisco",
     faction: "None",
     resource: "None",
   },
-  chihuahua: {
-    name: "Chihuahua",
+  michoacán: {
+    name: "Michoacán",
+    faction: "None",
+    resource: "None",
+  },
+  nayarit: {
+    name: "Nayarit",
+    faction: "None",
+    resource: "None",
+  },
+  campeche: {
+    name: "Campeche",
+    faction: "None",
+    resource: "None",
+  },
+  oaxaca: {
+    name: "Oaxaca",
+    faction: "None",
+    resource: "None",
+  },
+  puebla: {
+    name: "Puebla",
+    faction: "None",
+    resource: "None",
+  },
+  tabasco: {
+    name: "Tabasco",
+    faction: "None",
+    resource: "None",
+  },
+  tlaxcala: {
+    name: "Tlaxcala",
+    faction: "None",
+    resource: "None",
+  },
+  "distrito-federal": {
+    name: "Distrito Federal",
+    faction: "None",
+    resource: "None",
+  },
+  guanajuato: {
+    name: "Guanajuato",
+    faction: "None",
+    resource: "None",
+  },
+  guerrero: {
+    name: "Guerrero",
+    faction: "None",
+    resource: "None",
+  },
+  hidalgo: {
+    name: "Hidalgo",
+    faction: "None",
+    resource: "None",
+  },
+  méxico: {
+    name: "México",
+    faction: "None",
+    resource: "None",
+  },
+  morelos: {
+    name: "Morelos",
+    faction: "None",
+    resource: "None",
+  },
+  querétaro: {
+    name: "Querétaro",
+    faction: "None",
+    resource: "None",
+  },
+  veracruz: {
+    name: "Veracruz",
+    faction: "None",
+    resource: "None",
+  },
+  chiapas: {
+    name: "Chiapas",
+    faction: "None",
+    resource: "None",
+  },
+  "quintana-roo": {
+    name: "Quintana Roo",
+    faction: "None",
+    resource: "None",
+  },
+  yucatán: {
+    name: "Yucatán",
     faction: "None",
     resource: "None",
   },
 };
+
+function removeSelectedState() {
+  Array.from(document.getElementsByClassName("map-state")).forEach(
+    (element) => {
+      element.classList.remove("selected");
+    }
+  );
+}
+
+function stateClicked(id) {
+  removeSelectedState();
+  document.getElementById(id).classList.add("selected");
+  updateMapInfoModule(id);
+}
+
+function updateMapInfoModule(id) {
+  document.getElementById("map-info__title").innerHTML =
+    currentMapState[id].name;
+  document.getElementById("map-info__faction").innerHTML =
+    currentMapState[id].faction;
+  document.getElementById("map-info__resource").innerHTML =
+    currentMapState[id].resource;
+}
