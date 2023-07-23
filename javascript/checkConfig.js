819c443b7053765c8bd51f556e8d50b3877d36a7
Sander Verwimp <91965164+sanDer153@users.noreply.github.com>
2023-07-23 22:09:44 +0200

    add config checks

--- javascript/checkConfig.js
+++ javascript/checkConfig.js
@@ -1,5 +1,7 @@
 function checkInitialMapState() {
+  let stateCount = 0;
   for (const stateId in initialMapState) {
+    stateCount++;
     if (!resources.includes(initialMapState[stateId].resource)) {
       console.log(
         `ERROR: ${stateId} has an unknown resource '${initialMapState[stateId].resource}' assigned.`
@@ -13,10 +15,37 @@ function checkInitialMapState() {
       return;
     }
   }
+  if (stateCount != 32) {
+    console.log(
+      `ERROR: initialMapState has ${stateCount} states instead of 32.`
+    );
+    return;
+  }
+}
+
+function checkPolTechtree() {
+  const maxTechtreeLevel =
+    Object.keys(polTechtree)[Object.keys(polTechtree).length - 1];
+  for (let i = 0; i <= maxTechtreeLevel; i++) {
+    if (typeof polTechtree[i] === "undefined") {
+      console.log(`ERROR: level ${i} doesn't exist.`);
+      return;
+    }
+    if (
+      polTechtree[i].friendlyReferendumChance < 0 ||
+      polTechtree[i].friendlyReferendumChance > 1 ||
+      polTechtree[i].enemyReferendumModifier < 0 ||
+      polTechtree[i].enemyReferendumModifier > 1
+    ) {
+      console.log(`ERROR: chances have to be between 0 and 1.`);
+      return;
+    }
+  }
 }
 
 function runChecks() {
   checkInitialMapState();
+  checkPolTechtree();
 }
 
 runChecks();
